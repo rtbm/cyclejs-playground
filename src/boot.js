f1bc580ca5a3d17d91ee02c5efc1e85b41415773
rtbm <marcin@bogusz.cc>
2016-09-01 21:49:56 +0200

    bmi mvi

--- src/boot.js
+++ src/boot.js
@@ -3,31 +3,37 @@ require('rx');
 const Cycle =  require('@cycle/core');
 const { makeDOMDriver, div, input, label } = require('@cycle/dom');
 
-const main = sources => {
-  const heightChange$ = sources.DOM.select('.height').events('input').map(e => e.target.value);
-  const weightChange$ = sources.DOM.select('.weight').events('input').map(e => e.target.value);
+const intent = DOMSource => ({
+  heightChange$: DOMSource.select('.height').events('input').map(e => e.target.value),
+  weightChange$: DOMSource.select('.weight').events('input').map(e => e.target.value),
+});
 
-  const state$ = Rx.Observable.combineLatest(
-    heightChange$.startWith(80),
-    weightChange$.startWith(80),
-    (height, weight) => ({
-      height,
-      weight,
-      bmi: Math.round(weight / ((height * 0.01) * (height * 0.01)))
-    })
-  );
+const model = (heightChange$, weightChange$) => Rx.Observable.combineLatest(
+  heightChange$.startWith(80),
+  weightChange$.startWith(80),
+  (height, weight) => ({
+    height,
+    weight,
+    bmi: Math.round(weight / ((height * 0.01) * (height * 0.01)))
+  })
+);
+
+const view = state$ => state$.map(state => div([
+  div('.bmi', `BMI: ${state.bmi}`),
+  label(`Height: ${ state.height }`),
+  input('.height', { attrs: { type: 'range', min: 1, max: 350, value: state.height }}),
+  label(`Weight: ${ state.weight }`),
+  input('.weight', { attrs: { type: 'range', min: 1, max: 250, value: state.weight }}),
+]));
+
+const main = sources => {
+  const { heightChange$, weightChange$ } = intent(sources.DOM);
+  const state$ = model(heightChange$, weightChange$);
+  const $vtree = view(state$);
 
   return {
-    DOM: state$.map(state =>
-      div([
-        div('.bmi', `BMI: ${state.bmi}`),
-        label(`Height: ${ state.height }`),
-        input('.height', { attrs: { type: 'range', min: 1, max: 350, value: state.height }}),
-        label(`Weight: ${ state.weight }`),
-        input('.weight', { attrs: { type: 'range', min: 1, max: 250, value: state.weight }}),
-      ]),
-    ),
-  };
+    DOM: $vtree
+  }
 };
 
 const drivers = {
